ef2ac7ae57e3a614f9648bfdaa94df8decd0f98c
AdminGit2024 <haraledeepak6@gmail.com>
2024-11-30 22:44:06 +0530

    POM page creation completed

--- src/main/java/com/jala/qa/testbase/testBase.java
+++ src/main/java/com/jala/qa/testbase/testBase.java
@@ -2,26 +2,30 @@ package com.jala.qa.testbase;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.time.Duration;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 public class testBase {
 public static Properties prop;
 public static WebDriver driver;
-	testBase() throws IOException{
+public	testBase() throws IOException{
 		 prop = new Properties();
 		 String baseLocation = System.getProperty("user.dir");
 		FileInputStream file = new FileInputStream(baseLocation+"\\src\\main\\java\\com\\jala\\qa\\enviromentLayer\\config.properties");
 		prop.load(file);
 	}
-	
-
+	@Test
 	public void intilization() {
 		String Browser = prop.getProperty("Browsername");
 		if(Browser.equals("chrome")) {
@@ -38,13 +42,18 @@ public static WebDriver driver;
 		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
 		driver.get(prop.getProperty("Url"));
-		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-		
-		
+//		Implicit wait
+		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS); // 2sec = relese 13 sec
+//	explicit wait
+//		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30)); // 5 sec = relse 25 sec
+//		 WebElement username = driver.findElement(By.id("UserName"));
+//		 wait.until(ExpectedConditions.visibilityOf(username));
+		 
+		 
 	}
 	
 	public static void holdTime() throws InterruptedException {
-		Thread.sleep(3000);
+		Thread.sleep(3000);   // 2 sec => wait for 3 sec = 1 sec hold
 		
 	}
 	
